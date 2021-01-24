cd78a3051129b90b3f7a945fbf1e1afb2d0b8244
Heli Herrera <heli.herrera.dev@gmail.com>
2021-01-23 21:29:30 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,26 +7,27 @@ export default function Home() {
       <Head>
         <title>Logic Symmetry</title>
         <link rel="icon" href="/favicon.ico" />
+      
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
           Coming soon
         </h1>
+        <br></br>
 
         <p className={styles.description}>
         <img src="/LogicSymmetry.svg" alt="Logic Symmetry Logo" className={styles.logoTop} />Logic Symmetry{' '}
         </p>
         <p className={styles.code}>We are working on the full version of our new site and will be back shortly.</p>
-
         <div className={styles.grid}>
           <a href="mailto:heli.herrera.dev@gmail.com" target="_blank" className={styles.card}>
-            <h3><img src="https://logos-world.net/wp-content/uploads/2020/11/Gmail-Logo-700x394.png" alt="LogicSymmetry Logo" className={styles.logo} />Contact us &rarr;</h3>
+            <h3><img src="/Gmail-Logo.png" alt="LogicSymmetry Logo" className={styles.logo} />Contact us &rarr;</h3>
             <p>heli.herrera.dev@gmail.com</p>
           </a>
 
           <a href="https://twitter.com/Heli_Herrera" className={styles.card}>
-            <h3><img src="https://seeklogo.com/images/T/twitter-logo-7249D46199-seeklogo.com.png" alt="LogicSymmetry Logo" className={styles.logo} /> Twitter &rarr;</h3>
+            <h3><img src="/twitter-logo.png" alt="LogicSymmetry Logo" className={styles.logo} /> Twitter &rarr;</h3>
             <p>https://twitter.com/Heli_Herrera</p>
           </a>
 
@@ -34,7 +35,7 @@ export default function Home() {
             href="https://github.com/heliherrera"
             className={styles.card}
           >
-            <h3><img src="https://seeklogo.com/images/G/github-logo-7880D80B8D-seeklogo.com.png" alt="LogicSymmetry Logo" className={styles.logo} /> Github &rarr;</h3>
+            <h3><img src="/github-logo.png" alt="LogicSymmetry Logo" className={styles.logo} /> Github &rarr;</h3>
             <p>https://github.com/heliherrera.</p>
           </a>
 
@@ -43,7 +44,7 @@ export default function Home() {
             className={styles.card}
           >
              
-            <h3><img src="https://seeklogo.com/images/D/dribbble-logo-143FF96D65-seeklogo.com.png" alt="LogicSymmetry Logo" className={styles.logo} /> Dribbble &rarr;</h3>
+            <h3><img src="/dribbble-logo.png" alt="LogicSymmetry Logo" className={styles.logo} /> Dribbble &rarr;</h3>
             <p>
             https://dribbble.com/heliherrera
             </p>
@@ -64,4 +65,5 @@ export default function Home() {
     </div>
   )
 }
+
 
